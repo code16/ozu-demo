4516aa11486107c77c6063cb5c6037b2392632c1
philippe <philippe@code16.fr>
2024-02-06 18:48:47 +0100

    WIP

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Code16\JockoClient\Eloquent\JockoModel;
 use Code16\JockoClient\Eloquent\Media;
+use Code16\JockoClient\JockoCms\Form\JockoField;
 use Code16\JockoClient\JockoCms\JockoCollectionFormConfig;
 use Code16\JockoClient\JockoCms\JockoCollectionListConfig;
 use Code16\JockoClient\JockoCms\JockoCollectionConfig;
@@ -42,22 +43,24 @@ class Project extends JockoModel
             ->addColumn(JockoColumn::makeText('title', 4)->setLabel('Title'))
             ->addColumn(JockoColumn::makeText('reference', 3)->setLabel('Ref'))
             ->addColumn(JockoColumn::makeText('year', 3)->setLabel('Year'))
+            ->setIsSearchable()
             ->setIsReorderable();
     }
 
     public static function configureJockoCollectionForm(JockoCollectionFormConfig $config): JockoCollectionFormConfig
     {
         return $config
-//            ->addCustomField(
-//                JockoField::makeText('reference')
-//                    ->setLabel('Ref')
-//                    ->setValidationRules(['required'])
-//            )
-//            ->addCustomField(
-//                JockoField::makeText('year')
-//                    ->setLabel('Year')
-//                    ->setValidationRules(['required', 'integer'])
-//            )
+            ->addCustomField(
+                JockoField::makeText('reference')
+                    ->setLabel('Ref')
+                    ->setValidationRules(['required'])
+            )
+            ->addCustomField(
+                JockoField::makeText('year')
+                    ->setLabel('Year')
+                    ->setValidationRules(['required', 'integer'])
+                    ->setHelpMessage('Year of the project')
+            )
             ->addCustomField(
                 JockoField::makeImageList('visuals')
                     ->setLabel('Visuals')

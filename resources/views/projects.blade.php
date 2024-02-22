140db36fc86372e97ca62a5a9d20d2966f435c6c
philippe <philippe@code16.fr>
2024-02-22 15:52:43 +0100

    WIP

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -15,7 +15,7 @@
 <div class="relative">
     @foreach($projects as $project)
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="{{ $project->cover->thumbnail(250) }}" alt="Sunset in the mountains">
+            <img class="w-full" src="{{ $project->cover?->thumbnail(250) }}" alt="Sunset in the mountains">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">{{ $project->title }}</div>
                 <p class="text-gray-700 text-base">
